f3975bb5f8f9a256e079a569bb5cc1081f4a7fba
Georgiy <goshazvir@gmail.com>
2014-02-17 18:03:28 +0200

    load

--- js/common.js
+++ js/common.js
@@ -1,7 +1,34 @@
 $(document).ready(function() {
 	$('.js-two-coll').click(function(event) {
 		$("body").toggleClass("two-coll");
-		$(".js-news-hide").hide();
 		return false;
 	});
+
+	$('.js-search').click(function(event) {
+		$(this).parents(".header").addClass("is-search");
+	});
+	$(".js-search-close").click(function(event){
+		$(this).parents(".header").removeClass("is-search");
+	});
+
+
+
+	// $('.js-search').click(function(event) {
+	// 	if ($(this).hasClass('is-active')){
+	// 		$(this).removeClass("is-active");
+	// 	  $(this).html("поиск");
+	// 	  $(this).parents(".header").addClass("is-search");
+	// 	  $(".header__search").hide();
+	// 	  $(".header__left").show();
+	// 	  $(".header__center").show();
+	// 	}
+	//   else{
+	//  	 	$(this).addClass("is-active");
+	//     $(this).html("закрыть");
+	//     $(this).parents(".header").removeClass("is-search");
+	//     $(".header__search").show();
+	//     $(".header__left").hide();
+	//     $(".header__center").hide();
+	//   }
+	//  });
 });
